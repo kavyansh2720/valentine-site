917f76748a87424ff41fdff1f1f6c96863d5e6b6
kavyansh2720 <kavyansh2720@gmail.com>
2026-02-03 14:40:18 +0530

    Change the script.txt to script.js

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/yes")
+def yes():
+    return render_template("yes.html")
+
+@app.route("/letter")
+def letter():
+    return render_template("letter.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
